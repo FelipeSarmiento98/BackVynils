83e53f182ca3ee0bca1a4c3d3fd5d5e46001caf5
Jhoan Felipe Sarmiento Ortiz <jf.sarmiento23@uniandes.edu.co>
2025-04-27 16:36:44 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -36,7 +36,7 @@ import { AlbumMusicianModule } from './albummusician/albummusician.module';
   imports: [  
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
+      host: 'db',
       port: 5432,
       username: process.env.DB_USER || 'postgres',
       password: process.env.DB_PASS || 'postgres',
