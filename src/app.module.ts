e95c4016f879025128b75fe256f86697434561fd
Jhoan Felipe Sarmiento Ortiz <jf.sarmiento23@uniandes.edu.co>
2025-04-27 17:26:25 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -36,11 +36,11 @@ import { AlbumMusicianModule } from './albummusician/albummusician.module';
   imports: [  
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'backvynils.railway.internal',
-      port: 5432,
-      username: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASS || 'postgres',
-      database: process.env.DB_NAME || 'vinyls',
+      host: process.env.PGHOST || 'localhost',
+      port: parseInt(process.env.PGPORT, 10) || 5432,
+      username: process.env.PGUSER || 'postgres',
+      password: process.env.PGPASSWORD || 'postgres',
+      database: process.env.PGDATABASE || 'vinyls',
       entities: [Album, CollectorAlbum, Band, Collector, Comment, Musician, Performer, PerformerPrize, Prize, Track,],
       dropSchema: false,
       synchronize: true,
